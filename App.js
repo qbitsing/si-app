a3029be37303e20863fd92f31d22b1f4dd664853
Narias1999 <nicolasarias870@gmail.com>
2018-05-31 16:37:10 -0500

    navigation

--- App.js
+++ App.js
@@ -1,58 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Icon } from 'native-base'
+import { TabNavigator, TabBarBottom } from 'react-navigation'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+export default TabNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Categories: { screen: SettingsScreen },
+    New: { screen: SettingsScreen },
+    Sales: { screen: SettingsScreen },
+    Profile: { screen: SettingsScreen },
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName
+        if (routeName === 'Home') {
+          iconName = `home`
+        } else if (routeName === 'Categories') {
+          iconName = 'apps'
+        } else if (routeName === 'New') {
+          iconName = 'add'
+        } else if (routeName === 'Sales') {
+          iconName = 'cart'
+        } else if (routeName === 'Profile') {
+          iconName = 'person'
+        }
+        return <Icon name={iconName} style={{fontSize: 20, color: tintColor}} />
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
