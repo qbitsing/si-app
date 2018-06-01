99f4c52737dc54c1c5eb6199fa199570b818b720
Narias1999 <nicolasarias870@gmail.com>
2018-05-31 19:07:02 -0500

    card

--- views/Home.js
+++ views/Home.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import {Image} from 'react-native'
+import {Image, View, Text} from 'react-native'
 import {
   Content,
   Card,
-  Text,
   CardItem,
   Left,
   Icon,
   Body,
   Thumbnail,
+  H3,
+  H2,
   Button,
   Right
 } from 'native-base'
@@ -23,17 +24,20 @@ class Home extends Component {
             <Image source={{uri: 'https://www.setaswall.com/wp-content/uploads/2017/06/Material-Backgrounds-08-1366-x-768.png'}} style={{height: 180, width: null, flex: 1}}/>
           </Left>
           <Body>
-            <Text>
-              Holaa
-            </Text>
+              <Text>Tecnología > Celulares</Text>
+              <Text note>Celular nuevo, color negro con 32 gb de almacenamiento. 4 gb de RAM...</Text>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <H2>$750.000</H2>
+                <Text>username</Text>
+              </View>
           </Body>
           </CardItem>
           <CardItem>
               <Left>
-                <Button rounded style={{marginRight:20, backgroundColor: '#1b95e0'}}>
+                <Button style={{marginRight:10, backgroundColor: '#1b95e0'}}>
                   <Icon name="logo-twitter" />
                 </Button>
-                <Button rounded>
+                <Button>
                   <Icon name="logo-facebook" />
                 </Button>
               </Left>
