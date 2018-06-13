a661575de1a06630e017d80dc8defdb8af226d84
HugoPro1 <hugo.karreno@gmail.com>
2018-06-13 11:46:06 -0500

    Saler Segmets proceso 1

--- src/components/tabs/Saler.js
+++ src/components/tabs/Saler.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react'
-import {Container, Content, Text} from 'native-base'
+import {Container, Content, Text, Segment, Button} from 'native-base'
 import Footer from './../FooterTabs'
 
+
 class Saler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seg: 1
+    }
+  }
+
+  setSegment = (seg) => {
+    this.setState({
+      seg
+    })
+  }
+
   render() {
     return(
       <Container>
         <Content>
-          <Text>This is the saler view</Text>
+        <Segment>
+          <Button
+          active={this.state.seg === 1 ? true : false}
+          onPress={() => this.setSegment(1)}
+          first>
+            <Text>Puppies</Text>
+          </Button>
+          <Button
+          active={this.state.seg === 2 ? true : false}
+          onPress={() => this.setSegment(2)} 
+          last>
+            <Text>Kittens</Text>
+          </Button>
+        </Segment>
+        <Content padder>
+        {this.state.seg === 1 && <Text>Segment 1</Text>}
+          {this.state.seg === 2 && <Text>Segment 2</Text>}
+        </Content>
         </Content>
         <Footer navigation={this.props.navigation}/>
       </Container>
